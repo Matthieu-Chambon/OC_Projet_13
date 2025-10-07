2709491f0223acc607da618051ee20879afaefdc
Matthieu-Chambon <matthieu.chambon44@gmail.com>
2025-10-07 13:06:22 +0200

    Initialisation de la configuration Sphinx
    Added project information and Sphinx configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# --- Informations sur le projet ---
+project = "OC Projet 13"
+author = "Matthieu Chambon"
+release = "1.0"
+copyright = "%Y, Matthieu Chambon"
 
+# --- Chemin du projet pour les imports Python ---
+sys.path.insert(0, os.path.abspath(".."))
+
+# --- Extensions Sphinx ---
+extensions = [
+    "sphinx.ext.autodoc",      # Génération auto de doc à partir du code Python
+    "sphinx.ext.viewcode",     # Liens vers le code source
+    "myst_parser",             # Markdown
+]
+
+# --- Fichiers sources ---
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+# --- Fichier racine ---
+master_doc = "index"
+
+# --- Thème ---
+html_theme = "sphinx_rtd_theme"
