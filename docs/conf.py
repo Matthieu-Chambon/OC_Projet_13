21d6fd6b4ea3e1d865cce21742df20af1fb72a49
Matthieu-Chambon <matthieu.chambon44@gmail.com>
2025-10-07 13:14:13 +0200

    Suppression extension markdown
    Removed 'myst_parser' extension from Sphinx configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@ sys.path.insert(0, os.path.abspath(".."))
 extensions = [
     "sphinx.ext.autodoc",      # Génération auto de doc à partir du code Python
     "sphinx.ext.viewcode",     # Liens vers le code source
-    "myst_parser",             # Markdown
 ]
 
 # --- Fichiers sources ---
